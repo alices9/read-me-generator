dc0c33768ea470f21ca21942c4508739997d1b65
Alice Shao <a1ice_shao@yahoo.com>
2022-10-19 16:02:54 -0400

    added questions

--- index.js
+++ index.js
@@ -1,7 +1,67 @@
+// # <Your-Project-Title>
+
+// ## Description
+
+// Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+// - What was your motivation?
+// - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+// - What problem does it solve?
+// - What did you learn?
+
+// ## Table of Contents (Optional)
+
+// - [Installation](#installation)
+// - [Usage](#usage)
+// - [Credits](#credits)
+// - [License](#license)
+
+// ## Installation
+
+// What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+// ## Usage
+
+// Provide instructions and examples for use. Include screenshots as needed.
+
+// To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+//     ```md
+//     ![alt text](assets/images/screenshot.png)
+//     ```
+
+// ## Credits
+
+// List your collaborators, if any, with links to their GitHub profiles.
+
+// If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+
+// If you followed tutorials, include links to those here as well.
+
+// ## License
+
+// The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+
+
+
+
+
+
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is the name of your project?",
+    "Why did you build this project?",
+    "What problem does it solve?",
+    "What are the steps required to install your project?",
+    "How can someone use your project?",
+    "Do you have any collaborators?",
+    "What are their GitHub URLs?",
+    "Did you use a license?",
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
