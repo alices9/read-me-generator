13797e5b1311d38518d64beebf81d093313e9600
Alice Shao <a1ice_shao@yahoo.com>
2022-10-19 18:00:00 -0400

    added to the generateMarkdown function

--- index.js
+++ index.js
@@ -1,73 +1,70 @@
-// # <Your-Project-Title>
-
-// ## Description
-
-// Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-
-// - What was your motivation?
-// - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-// - What problem does it solve?
-// - What did you learn?
-
-// ## Table of Contents (Optional)
-
-// - [Installation](#installation)
-// - [Usage](#usage)
-// - [Credits](#credits)
-// - [License](#license)
-
-// ## Installation
-
-// What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
-
-// ## Usage
-
-// Provide instructions and examples for use. Include screenshots as needed.
-
-// To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-
-//     ```md
-//     ![alt text](assets/images/screenshot.png)
-//     ```
-
-// ## Credits
-
-// List your collaborators, if any, with links to their GitHub profiles.
-
-// If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-// If you followed tutorials, include links to those here as well.
-
-// ## License
-
-// The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-
-
-
-
-
-
-
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [
-    "What is the name of your project?",
-    "Why did you build this project?",
-    "What problem does it solve?",
-    "What are the steps required to install your project?",
-    "How can someone use your project?",
-    "Do you have any collaborators?",
-    "What are their GitHub URLs?",
-    "Did you use a license?",
+const questions = [{
+    type: "input",
+    message: "What is the name of your project?",
+    name: "title"
+},
+{
+    type: "input",
+    message: "Why did you build this project?",
+    name: "description1"
+},
+{
+    type: "input",
+    message: "What problem does it solve?",
+    name: "description2"
+},
+{
+    type: "input",
+    message: "What are the steps required to install your project?",
+    name: "installation"
+},
+{
+    type: "input",
+    message: "How can someone use your project?",
+    name: "usage"
+},
+{
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "username"
+},
+{
+    type: "input",
+    message: "What is your email address?",
+    name: "email"
+},
+{
+    type: "input",
+    message: "Did you have any collaborators?",
+    choiced: ["Yes", "No"],
+    name: "collaborators"
+},
+{
+    type: "input",
+    message: "If yes, what are their GitHub URLs? Skip question if no.",
+    name: "collabURL"
+},
+{
+    type: "input",
+    message: "Which license did you use if any?",
+    choices: [],
+    name: "license"
+}
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
+
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
+
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,67 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { 
+  return `- [License](#license)`
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
+
+function renderCollaborators(collaborators) {
+  if (collaborators !== "No"){
+    return (`## Credits
+    
+    ${data.collabURL}`)
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+## Description
+
+${data.description1} ${data.description2}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+${renderLicenseLink}
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+${renderCollaborators(data.collaborators)}
+
+// ## License
+
+// The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+## Questions
+
+My GitHub username is <a href="github.com/${data.username} target="_blank"> ${data.username}</a>. If you have any questions, feel free to send me an email at ${data.email}.
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+
+
+
+
+
+    `;
 }
 
 module.exports = generateMarkdown;
